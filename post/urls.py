5b959078a2e2a69a55bee0eb64356b42aed93278
ProgstN1 <136348306+ProgstN1@users.noreply.github.com>
2023-09-04 19:04:22 +0500

    first commit

---
+++ post/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from. import views
+
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     path('login/', views.login, name='login'),
+#     path('logout/', views.logout, name='logout'),
+#     path('register/', views.register, name='register'),
+#     path('profile/', views.profile, name='profile'),
+#     path('profile/edit/', views.edit_profile, name='edit_profile'),
+#     path('profile/edit/password/', views.edit_password, name='edit_password'),
+# ]
+
+urlpatterns = [
+]
---
+++ post/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# def index(request):
+#     return render(request, 'index.html')
+
+# def login(request):
+#     return render(request, 'login.html')
+
+# def logout(request):
+#     return render(request, 'logout.html')
+
+# def register(request):
+#     return render(request,'register.html')
+
+# def profile(request):
+#     return render(request, 'profile.html')
+
+# def edit_profile(request):
+#     return render(request, 'edit_profile.html')
+
+# def edit_password(request):
+#     return render(request, 'edit_password.html')
